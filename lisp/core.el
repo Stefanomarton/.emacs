e1b0482f001ffc628b150861cda63c59b81b66cb
Stefanomarton <sstefanomarton@gmail.com>
2024-05-01 01:36:50 +0200

    chore: small optmisation

--- lisp/core.el
+++ lisp/core.el
@@ -3,13 +3,29 @@
 ;;; Configuration for the "core" behavior of GNU Emacs. Generally, anything
 ;;; which does not involve a third-party package.
 
+;; Reduce rendering/line scan work for Emacs by not rendering cursors or regions
+;; in non-focused windows.
+(setq-default cursor-in-non-selected-windows nil)
+(setq highlight-nonselected-windows nil)
+
 ;; Having Emacs open to `*scratch*' feels as though it's inviting me to punch
 ;; out some Lisp forms and evaluate them. I like to pretend that my computers
 ;; are Lisp machines.
 (setq inhibit-startup-message t
       inhibit-startup-buffer-menu t
       initial-scratch-message nil
-      initial-buffer-choice nil)
+      initial-buffer-choice nil
+      inhibit-splash-screen t
+      inhibit-startup-echo-area-message t
+      inhibit-default-init t
+      initial-major-mode 'fundamental-mode)
+
+;; favor vertical splits over horizontal ones?
+(setq split-width-threshold 140
+      split-height-threshold 80)
+
+;; Show me what I type, immediately.
+(setq echo-keystrokes 0.01)
 
 ;; Disable useless messsages
 (setq inhibit-message nil)
@@ -121,11 +137,14 @@
 ;; just scroll by one line.
 
 ;; Better then global centered cursor mode
-(setq scroll-conservatively most-positive-fixnum
+(setq scroll-conservatively 101
       indicate-empty-lines nil
       scroll-preserve-screen-position t
-      scroll-margin 1000
-      maximum-scroll-margin 0.5)
+      scroll-margin 10
+      maximum-scroll-margin 0.5
+      next-screen-context-lines 5
+      auto-window-vscroll nil
+      )
 
 ;; always follow symlinks when opening files
 (setq vc-follow-symlinks t)
@@ -263,5 +282,52 @@
 
 ;; Overwrite selection on pasting
 (delete-selection-mode 1)
+
+(use-package goto-chg
+  :ensure t
+  :bind (("<escape> ;" . goto-last-change)
+         ("<escape> :" . goto-last-change-reverse)))
+
+;;;###autoload
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (if (vc-registered filename)
+            (vc-rename-file name new-name)
+          (rename-file name new-name 1))
+        (rename-buffer new-name)
+        (set-visited-file-name new-name)
+        (set-buffer-modified-p nil)))))
+
+;;;###autoload
+(defun move-buffer-file (dir)
+  "Moves both current buffer and file it's visiting to DIR."
+  (interactive "DNew directory: ")
+  (let* ((name (buffer-name))
+         (filename (buffer-file-name))
+         (dir
+          (if (string-match dir "\\(?:/\\|\\\\)$")
+              (substring dir 0 -1) dir))
+         (newname (concat dir "/" name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (progn (copy-file filename newname 1)
+             (delete-file filename)
+             (set-visited-file-name newname)
+             (set-buffer-modified-p nil) t))))
+
+(use-package compile
+  :defer t
+  :hook (compilation-filter . ansi-color-compilation-filter)
+  :config
+  (setq compilation-always-kill t
+        compilation-ask-about-save nil
+        compilation-scroll-output 'first-error))
 
 (provide 'core)
