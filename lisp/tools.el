4e5f6002b0d86b9ca66d92b612780b75c833706c
Stefanomarton <sstefanomarton@gmail.com>
2024-06-17 21:52:44 +0200

    feat: fix-word, popper and zoxide

--- lisp/tools.el
+++ lisp/tools.el
@@ -8,21 +8,33 @@
   ([remap describe-variable] . helpful-variable)
   ([remap describe-function] . helpful-function))
 
+(use-package fix-word
+  :bind
+  (([remap upcase-word] . fix-word-Upcase))
+  (([remap downcase-word] . fix-word-downcase))
+  (([remap capitalize-word] . fix-word-capitalize))
+  )
+
+(use-package altcaps
+  :commands altcaps)
+
 ;; Startup time evaluation
 (use-package esup
   :commands esup)
 
-(use-package vterm
-  :commands vterm
-  :config
-  ;; (set-fontset-font t 'unicode (font-spec :family "JetBrainsMono Nerd Font"))
-  :custom
-  (setq term-toggle-no-confirm-exit t)
-  (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *"
-	    vterm-internal-use-ligatures t
-	    vterm-max-scrollback 10000
-	    vterm-shell "zsh"
-	    ))
+;; (use-package vterm
+;;   :commands vterm
+;;   :bind (:map global-map
+;;               ("<escape>v" . vterm-other-window))
+;;   :config
+;;   ;; (set-fontset-font t 'unicode (font-spec :family "JetBrainsMono Nerd Font"))
+;;   :custom
+;;   (setq term-toggle-no-confirm-exit t)
+;;   (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *"
+;; 	    vterm-internal-use-ligatures t
+;; 	    vterm-max-scrollback 10000
+;; 	    vterm-shell "zsh"
+;; 	    ))
 
 (use-package ellama
   :init
@@ -47,11 +59,11 @@
 (use-package explain-pause-mode
   :defer t)
 
-(use-package golden-ratio
-  :hook after-init
-  :config
-  (setq golden-ratio-exclude-modes '(emacs-lisp-compilation-mode)) ; Exclude modes from `golder-ratio-mode'
-  (golden-ratio-mode))
+;; (use-package golden-ratio
+;;   :hook after-init
+;;   :config
+;;   (setq golden-ratio-exclude-modes '(emacs-lisp-compilation-mode)) ; Exclude modes from `golder-ratio-mode'
+;;   (golden-ratio-mode))
 
 (use-package winner
   :hook after-init
@@ -59,11 +71,33 @@
   (winner-mode))
 
 (use-package ebuku
-  :config
-  (setq ebuku-database-path (expand-file-name "~/GoogleDrive/Projects/Linux/buku/buku.db")))
+  :commands ebuku)
+
 
 (use-package sudo-edit
   :commands sudo-edit)
+
+(use-package popper
+  :ensure t ; or :straight t
+  :bind (("<escape>,"   . popper-toggle)
+         ("<escape>."   . popper-cycle)
+         ("C-M-`" . popper-toggle-type))
+  :init
+  (setq popper-reference-buffers
+        '("\\*Messages\\*"
+          "Output\\*$"
+          "\\*Async Shell Command\\*"
+          help-mode
+          compilation-mode))
+  (popper-mode +1)
+  (setq popper-group-function #'popper-group-by-projectile)
+  ;; (popper-echo-mode +1)
+  ;; Match eshell, shell, term and/or vterm buffers
+  )
+
+(use-package zoxide
+  :bind (:map global-map)
+  ("<escape>z" . zoxide-find-file))
 
 (provide 'tools)
 
