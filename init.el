bf9f58d65d431502d9d0c1969158c91040b23927
Stefanomarton <sstefanomarton@gmail.com>
2025-03-28 20:46:00 +0100

    chore: update elpaca installer version

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 
 (setq elpaca-core-date '(20241219))
 
-(defvar elpaca-installer-version 0.9)
+(defvar elpaca-installer-version 0.10)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
