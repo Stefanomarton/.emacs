085ed108165c20750f12e816b452ed4c318e3667
Stefanomarton <sstefanomarton@gmail.com>
2025-06-17 19:05:23 +0200

    fix: typst repo

--- lisp/typst.el
+++ lisp/typst.el
@@ -2,14 +2,19 @@
 
 (use-package typst-ts-mode
   :ensure t
-  :vc (:url "https://git.sr.ht/~meow_king/typst-ts-mode"
+  :vc (:url "https://codeberg.org/meow_king/typst-ts-mode"
             :branch "main")
+  :bind (:map typst-ts-mode-map
+              ("C-c <tab>" . outline-cycle))
+
+  :hook
+  (typst-ts-mode . outline-indent-mode)
   :config
   (setq typst-ts-output-directory "/tmp/pdf")
-  (setq typst-ts-compile-options "--root=/home/sm/.marton-drive/notes/")
-  )
+  (setq typst-ts-compile-options "--root=$HOME/.marton-drive/notes/"))
 
-(use-package outline-indent
+(use-package outline-indent-mode
+  :vc (:url "https://git.sr.ht/~meow_king/outline-indent-mode" :branch "main")
   :ensure t)
 
 ;;; typst.el ends here
