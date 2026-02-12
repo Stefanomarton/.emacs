4c30ef5d9ef329694e7cab223fe618f7dc649a51
Stefanomarton <sstefanomarton@gmail.com>
2026-02-12 14:35:15 +0100

    fix: delete useless scripts

--- snippets/typst-ts-mode/.yas-compiled-snippets.el
+++ snippets/typst-ts-mode/.yas-compiled-snippets.el
@@ -2,29 +2,30 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'typst-ts-mode
-                     '(("quote"
-                        "#quote(attribution: [$1])[\n$2\n]\n$0"
-                        "quote" nil nil nil
-                        "/home/stefanom/.config/emacs/snippets/typst-ts-mode/quote"
-                        nil nil)
-                       ("lorem" "#lorem($1)$0" "lorem" nil nil nil
-                        "/home/stefanom/.config/emacs/snippets/typst-ts-mode/lorem"
-                        nil nil)
-                       ("//" "($1)/($2)" "frac" nil nil nil
-                        "/home/stefanom/.config/emacs/snippets/typst-ts-mode/frac"
-                        nil nil)
-                       ("wideblock"
+                     '(("wideblock"
                         "#wideblock(\n$0\ndouble: true,\n)"
                         "wideblock" nil nil nil
-                        "/home/stefanom/.config/emacs/snippets/typst-ts-mode/figure"
+                        "/home/sm/.config/emacs/snippets/typst-ts-mode/wideblock"
                         nil nil)
-                       ("equation" "$\n$0\n$" "equation" nil nil nil
-                        "/home/stefanom/.config/emacs/snippets/typst-ts-mode/equation"
+                       ("quote"
+                        "#quote(attribution: [$1])[\n$2\n]\n$0"
+                        "quote" nil nil nil
+                        "/home/sm/.config/emacs/snippets/typst-ts-mode/quote"
+                        nil nil)
+                       ("lorem" "#lorem($1)$0" "lorem" nil nil nil
+                        "/home/sm/.config/emacs/snippets/typst-ts-mode/lorem"
+                        nil nil)
+                       ("grid" "#grid(\ncolumns:(50%,50%),\n$0\n)\n"
+                        "grid" nil nil nil
+                        "/home/sm/.config/emacs/snippets/typst-ts-mode/grid"
+                        nil nil)
+                       ("//" "($1)/($2)" "frac" nil nil nil
+                        "/home/sm/.config/emacs/snippets/typst-ts-mode/frac"
                         nil nil)
                        ("align" "#align($1)[\n$2\n]$0" "align" nil nil
                         nil
-                        "/home/stefanom/.config/emacs/snippets/typst-ts-mode/align"
+                        "/home/sm/.config/emacs/snippets/typst-ts-mode/align"
                         nil nil)))
 
 
-;;; Do not edit! File generated at Tue Jun 17 17:26:33 2025
+;;; Do not edit! File generated at Wed Dec 17 21:35:20 2025
